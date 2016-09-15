426547453a128976b04097d529151f7fbef7bc20
Peter <p@m8by.me>
2016-09-16 00:37:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,28 @@
 <?php
-
 include_once("conn.php"); 
-
 $results = $mysqli->query("SELECT id, content, timestamp FROM postDB");
-
 while($row = $results->fetch_assoc())	{
-   $resultPackage .= '<li id="item_'.$row["id"].'">
-					 <p class="tStamp">'. $row["timestamp"] . '</p>
-					 <div class="del_wrapper"><a href="#" class="del_button" id="del-'.$row["id"].'">
-					 <img src="images/icon_del.gif" border="0" />
-					 </a></div>
-					 <p class="besked">' . $row["content"] . '</p></li>';
-					
-
+	$resultPackage .= '
+		<li id="item_'.$row["id"].'">
+			<p class="tStamp">'. $row["timestamp"] . '</p>
+			<div class="del_wrapper">
+				<a href="#" class="del_button" id="del-'.$row["id"].'">
+					<img src="images/icon_del.gif" border="0" />
+				</a>
+			</div>
+			<p class="besked">' . $row["content"] . '</p>
+		</li>
+	';
 }
-
 $mysqli->close();
-							
-							
 ?>
-
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>POST</title>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-		
 		<script type="text/javascript">
 			$(document).ready(function() {
-
 				$("#FormSubmit").click(function (e) {
 					e.preventDefault();
 					if($("#contentText").val()==='')
@@ -36,23 +30,19 @@ $mysqli->close();
 				alert("Indtast noget!");
 				return false;
 			}
-			
 			$("#FormSubmit").hide(); 
 			$("#LoadingImage").show(); 
-			
 		 	var myData = 'content_txt='+ $("#contentText").val(); 
 			jQuery.ajax(	{
 				type: "POST", 
 				url: "handler.php", 
 				dataType:"text", 
 				data:myData, 
-				success:function(response){
+				success:function(response)	{
 					$("#responds").append(response);
 					$("#contentText").val(''); 
 					$("#FormSubmit").show(); 
 					$("#LoadingImage").hide();
-					
-					
 				},
 				error:function (xhr, ajaxOptions, thrownError)	{
 					$("#FormSubmit").show(); 
@@ -62,42 +52,33 @@ $mysqli->close();
 			});
 			
 		});
-
-	
-	$("body").on("click", "#responds .del_button", function(e) {
-		e.preventDefault();
-		var clickedID = this.id.split('-'); 
-		var DbNumberID = clickedID[1]; 
-		var myData = 'recordToDelete='+ DbNumberID; 
-		 
-		$('#item_'+DbNumberID).addClass( "sel" ); 
-		$(this).hide();  
-		jQuery.ajax(	{
-			type: "POST",
-			url: "handler.php", 
-			dataType:"text", 
-			data:myData, 
-			
-			success:function()	{
-				$('#item_'+DbNumberID).fadeOut();
-			},
-			error:function (xhr, ajaxOptions, thrownError)	{
-				alert(thrownError);
-			}
+		$("body").on("click", "#responds .del_button", function(e) {
+			e.preventDefault();
+			var clickedID = this.id.split('-'); 
+			var DbNumberID = clickedID[1]; 
+			var myData = 'recordToDelete='+ DbNumberID; 
+			$('#item_'+DbNumberID).addClass( "sel" ); 
+			$(this).hide();  
+			jQuery.ajax(	{
+				type: "POST",
+				url: "handler.php", 
+				dataType:"text", 
+				data:myData, 
+				success:function()	{
+					$('#item_'+DbNumberID).fadeOut();
+				},
+				error:function (xhr, ajaxOptions, thrownError)	{
+					alert(thrownError);
+				}
+			});
 		});
 	});
-});
-
 </script>
-		
 <link href="style.css" rel="stylesheet" type="text/css" />
-
 </head>
-	
 <body>
 	<div class="content_wrapper">
 		<ul id="responds"> <?php echo $resultPackage;  ?> </ul>
-
 		<div class="form_style">
 			<textarea name="content_txt" id="contentText" cols="54" rows="8" placeholder="Skriv noget!"></textarea>
 			<button id="FormSubmit" action="./">Go Go</button>
